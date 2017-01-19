917aec1333c2149815257b7242ca22d78a7fd60e
Zhe Yan <yzenithcorp@gmail.com>
2017-01-18 22:08:46 -0500

    add gulp wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
+var browserSync = require('browsersync');
 // var minify = require('gulp-minify-css');
 
 // var imagemin = require('gulp-imagemin');
@@ -15,23 +17,41 @@ var prefix = require('gulp-autoprefixer');
 //     }))
 //     .pipe(gulp.dest('dist/images'));
 // });
+gulp.task('browser-sync',function({
+	browser-sync({
+		server:{
+			baseDir: '..'
+		}
+	});
+}));
+
+gulp.task('build',function(){
+	gulp.src("pages/*.html")
+		.pipe(wrap({src:"layout/default.html"}))
+		.pipe(gulp.dest(".."))
+});
+
+function handleError(err){
+	console.log(err.toString());
+	this.emit('end');
+}
 
 gulp.task('sass',function(){
 	gulp.src('styles/main.scss')
-		.pipe(sass())
+		.pipe(sass()).on('error',handleError)
 		.pipe(prefix())
 		// .pipe(minify())
 		.pipe(gulp.dest('../styles'));
 });
 
-gulp.task('copy-assets', function(){
-  gulp.src('index.html')
-      .pipe(gulp.dest('../'));
-});
+// gulp.task('copy-assets', function(){
+//   gulp.src('index.html')
+//       .pipe(gulp.dest('../'));
+// });
 
 gulp.task('watch',function(){
-  gulp.watch(['*.html'],['copy-assets']);
+  gulp.watch(['**/*.html'],['build']);
   gulp.watch(['styles/*.scss'],['sass']);
 })
 
-gulp.task('default', ['sass', 'copy-assets','watch']);
+gulp.task('default', ['sass', 'build','watch']);
