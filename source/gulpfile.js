b73cacd2f38c808c8f2a7e08bd81f767cffd7e6d
Zhe Yan <yzenithcorp@gmail.com>
2017-01-20 20:54:39 -0500

    set browserSync

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 var wrap = require('gulp-wrap');
-var browserSync = require('browsersync');
+var browserSync = require('browser-sync').create();
 // var minify = require('gulp-minify-css');
 
 // var imagemin = require('gulp-imagemin');
@@ -17,41 +17,54 @@ var browserSync = require('browsersync');
 //     }))
 //     .pipe(gulp.dest('dist/images'));
 // });
-gulp.task('browser-sync',function({
-	browser-sync({
-		server:{
-			baseDir: '..'
-		}
-	});
-}));
 
+// browser-sync
+gulp.task('browser-sync',['build','sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: ".."
+        }
+    });
+});
+
+// add wrap
 gulp.task('build',function(){
 	gulp.src("pages/*.html")
 		.pipe(wrap({src:"layout/default.html"}))
 		.pipe(gulp.dest(".."))
 });
 
+// report error
 function handleError(err){
 	console.log(err.toString());
 	this.emit('end');
 }
 
+// sass
 gulp.task('sass',function(){
-	gulp.src('styles/main.scss')
+	return gulp.src('styles/main.scss')
 		.pipe(sass()).on('error',handleError)
 		.pipe(prefix())
 		// .pipe(minify())
-		.pipe(gulp.dest('../styles'));
+		.pipe(gulp.dest('../styles'))
+		.pipe(browserSync.reload({stream:true}));
 });
+
 
 // gulp.task('copy-assets', function(){
 //   gulp.src('index.html')
 //       .pipe(gulp.dest('../'));
 // });
 
+// rebuild
+gulp.task('rebuild',['build'],function(){
+	browserSync.reload();
+});
+
+// watch
 gulp.task('watch',function(){
-  gulp.watch(['**/*.html'],['build']);
+  gulp.watch(['**/*.html'],['rebuild']);
   gulp.watch(['styles/*.scss'],['sass']);
 })
 
-gulp.task('default', ['sass', 'build','watch']);
+gulp.task('default', ['browser-sync','watch']);
