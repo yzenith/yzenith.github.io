0e5710cb4382877744b5d3d4ca83395e223b65c0
Zhe Yan <yzenithcorp@gmail.com>
2017-02-20 22:48:35 -0500

    modify index.html skills slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-window.onload = function(){
-  
-}
+$(function(){
+  var c = 0;
+  function autoRun(){
+    c++;
+
+    (c==8)?c=0:c;
+
+    // show up photos
+    $('.detail>img').eq(c).fadeIn(800).siblings('img').hide();
+  }
+
+  // set timer
+  timer = setInterval(autoRun,2500);
+
+    // $('.detail').mouseover(function(){
+    //   clearInterval(timer);
+    //
+    //   var c = $(this).index();
+    //
+    //   $('.detail>img').eq(c).show().siblings('img').hide();
+    // })
+
+  // })
+})
--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,29 @@
-window.onload = function(){
-  
-}
+
+$(function(){
+  var c = 0;
+
+  function autoRun(){
+    c++;
+
+    (c==8)?c=0:c;
+
+    // show up photos
+    $('.skills>img').eq(c).fadeIn(400).siblings('img').hide()
+  }
+
+  // set timer
+  timer = setInterval(autoRun,1000);
+
+    $('.skills').mouseover(function(){
+    clearInterval(timer);
+
+    var c = $(this).index();
+
+    $('.skills>img').eq(c).show().siblings('.skills>img').hide();
+  })
+
+  //鼠标移出的效果，这里面有个效果不对，就是移出时，会从往后的li开始继续轮播，有时又没有
+  $('.skills').mouseout(function(){
+    timer = setInterval(autoRun,1000);
+  })
+})
